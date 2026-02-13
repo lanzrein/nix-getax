7f84fb9b5607896eb9d7d7846c1db55d6e1fb1c4
Johan Lanzrein <lanzrein@protonmail.com>
2026-02-13 22:42:46 +0100

    chore: fmt

--- flake.nix
+++ flake.nix
@@ -2,70 +2,75 @@
   description = "Getax 2025 application";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url  = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = import nixpkgs {
-          inherit system ;
+          inherit system;
         };
 
         getax-pp-2025 = pkgs.stdenv.mkDerivation rec {
           pname = "getax-pp-2025";
           version = "2025";
-          
+
           src = pkgs.fetchurl {
             url = "http://www.getax.ch/getaxpp/2025/getax2025.tar.gz";
             sha256 = "0rlbssaxia5mgx4b2r0rxc58ll75n5i45pn7nl11481cq3lf4rfw";
           };
-          
-          nativeBuildInputs = [ pkgs.makeWrapper ];
-          
+
+          nativeBuildInputs = [pkgs.makeWrapper];
+
           buildInputs = [
             pkgs.jdk17
             pkgs.webkitgtk_4_1
             pkgs.gtk3 # display help
             pkgs.gobject-introspection
-            pkgs.gsettings-desktop-schemas  # saving files
+            pkgs.gsettings-desktop-schemas # saving files
           ];
-          
+
           unpackPhase = ''
             mkdir -p source
             ${pkgs.gnutar}/bin/tar -xzvf $src -C source
           '';
-          
+
           installPhase = ''
             mkdir -p $out/libexec/getax-pp-2025 $out/bin
             cp -r source/getax-pp-2025/* $out/libexec/getax-pp-2025/
-            
+
             # Fix the launcher script
-            # TL;DR : https://nix.dev/permalink/stub-ld 
-            # Maybe there is a different and better way ? 
+            # TL;DR : https://nix.dev/permalink/stub-ld
+            # Maybe there is a different and better way ?
             sed 's/jre\/bin\///' $out/libexec/getax-pp-2025/getax-pp-2025 > $out/libexec/getax-pp-2025/getax-pp-2025.fixed
             chmod +x $out/libexec/getax-pp-2025/getax-pp-2025.fixed
-            
+
             # Create wrapper script with proper library paths
             # Ensures that the jre can find the lib especially SWT related
             makeWrapper $out/libexec/getax-pp-2025/getax-pp-2025.fixed $out/bin/getax-pp-2025 \
               --chdir $out/libexec/getax-pp-2025 \
-              --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.jdk17 ]} \
-              --prefix LD_LIBRARY_PATH : ${pkgs.lib.makeLibraryPath [ 
-                pkgs.webkitgtk_4_1 
-                pkgs.gtk3 
-                pkgs.gobject-introspection
-                pkgs.glib
-                pkgs.cairo
-                pkgs.pango
-                pkgs.gdk-pixbuf
-                pkgs.atk
-              ]} \
+              --prefix PATH : ${pkgs.lib.makeBinPath [pkgs.jdk17]} \
+              --prefix LD_LIBRARY_PATH : ${pkgs.lib.makeLibraryPath [
+              pkgs.webkitgtk_4_1
+              pkgs.gtk3
+              pkgs.gobject-introspection
+              pkgs.glib
+              pkgs.cairo
+              pkgs.pango
+              pkgs.gdk-pixbuf
+              pkgs.atk
+            ]} \
               --prefix XDG_DATA_DIRS : ${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name} \
               --prefix XDG_DATA_DIRS : ${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}
-            '';
-          
+          '';
+
           meta = with pkgs.lib; {
             description = "Getax 2025 tax software";
             homepage = "http://www.getax.ch";
@@ -74,11 +79,10 @@
           };
         };
       in
-      with pkgs;
-      {
-        packages.default = getax-pp-2025;
-        apps.default = flake-utils.lib.mkApp { drv = getax-pp-2025; };
-        
-      }
+        with pkgs; {
+          packages.default = getax-pp-2025;
+          apps.default = flake-utils.lib.mkApp {drv = getax-pp-2025;};
+          formatter = nixpkgs.legacyPackages.x86_64-linux.alejandra;
+        }
     );
 }
